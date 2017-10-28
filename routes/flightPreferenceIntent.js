cdddbec2c5d2e29d132c29bf5b5eaa3a1342faeb
ajay <ajaymodi12@gmail.com>
2017-10-27 19:36:46 -0700

    ITA-57 Beta conversion
    preferences

--- routes/flightPreferenceIntent.js
+++ routes/flightPreferenceIntent.js
@@ -10,10 +10,38 @@ exports.flightPreference = function(){
     	this.attributes['airline_time']=this.event.request.intent.slots.airline_time.value;
     	this.attributes['food_cuisine']=this.event.request.intent.slots.food_cuisine.value;
     	this.attributes['food_type']=this.event.request.intent.slots.food_type.value;
+    	var user=this.attributes['mongo_user'];
+    	user.preferences.flight.airline_name=this.attributes['airline_name'];
+    	user.preferences.flight.airline_days=this.attributes['airline_days'];
+    	user.preferences.flight.airline_class=this.attributes['airline_class'];
+    	user.preferences.flight.airline_time=this.attributes['airline_time'];
+    	user.preferences.flight.food_cuisine=this.attributes['food_cuisine'];
+    	user.preferences.flight.food_type=this.attributes['food_type'];
+        var url = "http://ainuco.ddns.net:4324/users/"+profile.email;
+
     	
     	
     	console.log("flight pref : >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n"+JSON.stringify(this.attributes));
     	//TO DO add mongo update
+    	
+    	request({
+            url: url,
+            method: "POST",
+            json: true,
+			body:user
+		}, function (error, response, body) {
+        	console.log(body);
+            if (!error && response.statusCode == 200) {
+                console.log("response----->" + JSON.stringify(response));
+                console.log("body----->" + JSON.stringify(body));
+                mongoUser = body;
+                speechText = "Airline name as "+mongoUser.preferences.flight.airline_name+" has updated successfully";
+                console.log("speechText------------>"+speechText);
+                this.emit(':tell', speechText);
+            }else{
+                console.log("error----->" + error);
+			}
+        }.bind(this));
     	var speechText="Flight preferences updated. " +
     			"Do you also want to update Hotel or car preferences, " +
     			"if yes then say update car prefrences or update hotel preferences.";
